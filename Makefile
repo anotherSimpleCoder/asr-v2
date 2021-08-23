96d923d7d007cd5c37f95ebe28e97c335d505442
anotherSimpleCoder <71701743+anotherSimpleCoder@users.noreply.github.com>
2021-08-23 19:07:58 +0200

    Mac library finished

---
+++ Makefile
@@ -0,0 +1,23 @@
+SDL_FLAGS = -I /usr/local/include/SDL2 -D_REENTRANT 
+
+CFLAGS = $(SDL_FLAGS) -I include
+
+OPTIONS = -fsanitize=address
+
+LIBS = -lSDL2
+
+BIN_NAME = test.dylib
+
+compile:
+	g++ $(CFLAGS) -c src/*.cpp $(OPTIONS)
+	#g++ $(CFLAGS) -c main.cpp -o main.o $(OPTIONS)
+	g++ $(LIBS) -dynamiclib -fPIC -o $(BIN_NAME) *.o $(OPTIONS)
+	rm *.o
+
+remove:
+	rm $(BIN_NAME)
+	clear
+
+clean:
+	rm *.o
+	clear
---
+++ src/asrVector.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "asrVector.h"
+#include "asrDefines.h"
+
+using namespace std;
+
+asrVector::asrVector(int pX, int pY){
+	x = pX;
+	y = pY;
+}
+
+int asrVector::getPosition(int component){
+	switch(component){
+		case ASR_X:
+			return x;
+
+		case ASR_Y:
+			return y;
+	}
+}
+
+void asrVector::setPosition(int component, int value){
+	switch(component){
+		case ASR_X:
+			x = value;
+			break;
+
+		case ASR_Y:
+			y = value;
+			break;
+	}
+}
